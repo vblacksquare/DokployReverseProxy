8926c7bfc48d625e6a34db34a78e5c8b7e6023c3
vblacksquare <>
2025-04-29 17:12:43 +0300

    init

--- main.py
+++ main.py
@@ -22,7 +22,3 @@ async def redirect_all(path: str, request: Request):
         return RedirectResponse(url=f"/{BASEURL}/{path}")
 
     return RedirectResponse(url=path)
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
